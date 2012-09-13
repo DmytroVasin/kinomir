66ce4e6fc28b1a3ffd71f2c4ec43ae683f5122b3
DemitriyDN <vasindima779@gmail.com>
2012-09-13 08:34:50 +0300

    modified rescue_from in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  	rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
+
+
+	private
+    	def record_not_found
+  		  flash[:alert] = "page not found"
+  		  redirect_to root_path
+  		end
 end
